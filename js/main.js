b14ca7287fd5d6047c8faf3ecb42e48b41f13553
Lishia Ellis <lishia.ellis09@gmail.com>
2023-03-23 18:22:36 -0400

    changes made on 3.23.23

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+// let endTime = document.getElementById('end-time').value
+
+let endDisplay = document.getElementById('end')
+// let userInput = document.getElementById('start')
+// console.log(endDisplay)
+
+let time = 0
+// function increase12(){
+//  time += startTime + 12
+//  endTime.innerText =time
+// }
+
+function addto(){
+    let startTime = document.getElementById('start').value
+    console.log(startTime)
+    // time += startTime + 12
+    endDisplay.innerText = startTime
+    // console.log(endDisplay)
+}
+
+// function addto(){
+//     let startTime = Number(document.getElementById('start').value)
+//     console.log(startTime)
+//     time += startTime + 12
+//     endDisplay.innerText = time
+//     console.log(endDisplay)
+// }
+
+//TODO check out date method it starts the timer from when they click it
