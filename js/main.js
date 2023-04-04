f69992d25e5341d8755a530ffb3e14e476c3f1a4
Lishia Ellis <lishia.ellis09@gmail.com>
2023-04-04 13:49:54 -0400

    fixed 12hour function

--- js/main.js
+++ js/main.js
@@ -23,35 +23,21 @@ function addTo(){
    
     return (hourTime, minTime)
  }
-    
 
 function addTwelve(){
    addTo(hourTime, minTime)
    let newFast = hourTime + 12
+    
+    console.log(newFast)
+    if(newFast > 12){
+       newFast = newFast-12 + 'PM'
+    }
     hourDisplay.innerText = newFast
     minDisplay.innerText = minTime
     console.log(newFast)
-    if(newFast > 12){
-       hourDisplay =  newFast-12
-    }
-    // console.log(hourTime)
-    // console.log(minTime)
+    
 }
 
-// function addTo(){
-//     let startTime = document.getElementById('start').value
-//     console.log(startTime)
-//     // time += startTime + 12
-//     endDisplay.innerText = startTime
-//     // console.log(endDisplay)
-// }
 
-// function addto(){
-//     let startTime = Number(document.getElementById('start').value)
-//     console.log(startTime)
-//     time += startTime + 12
-//     endDisplay.innerText = time
-//     console.log(endDisplay)
-// }
 
 //TODO check out date method it starts the timer from when they click it
